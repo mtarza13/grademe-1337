be00836962f2f3983ebfce4db7336c7dafe94c14
mr_boot <152234120+Tboooot@users.noreply.github.com>
2025-01-21 22:07:35 +0100

    Update tester.sh

--- .resources/rank02/level3/sort_int_tab/tester.sh
+++ .resources/rank02/level3/sort_int_tab/tester.sh
@@ -23,5 +23,5 @@ file2=../../../../rendu/sort_int_tab/sort_int_tab.c
 
 
     rm out1 out2 out1.txt out2.txt 2>/dev/null
-    echo "$(tput setaf 2)$(tput bold)SUCCESS$(tput sgr 0)"
+    echo "$(tput setaf 2)$(tput bold)3zwaa$(tput sgr 0)"
     exit 1
